8f4a96783a3f62cc03be80583d5f2be2eff2dcfb
y_rawat <y_rawat@BEAVER.ENCS.concordia.ca>
2015-07-20 14:13:43 -0400

    Publishserver class

---
+++ DRMS_WEB_Service/src/server/PublishServer.java
@@ -0,0 +1,36 @@
+package server;
+
+
+import java.io.IOException;
+
+import javax.xml.ws.Endpoint;
+import javax.xml.ws.WebServiceRef;
+
+public class PublishServer extends Thread{
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		//@WebServiceRef(wsdlLocation="http://localhost:8080/cal?wsdl")
+		LibraryServer webLib1=new LibraryServer("Concordia",50001);
+        new Thread(webLib1).start();
+        
+        LibraryServer webLib2=new LibraryServer("Ottawa",50002);
+        new Thread(webLib2).start();
+        
+        LibraryServer webLib3=new LibraryServer("Waterloo",50003);
+        new Thread(webLib3).start();
+        
+		Endpoint endpoint1=Endpoint.publish("http://localhost:50001/cal", webLib1);
+		Endpoint endpoint2=Endpoint.publish("http://localhost:50002/cal", webLib2);
+		Endpoint endpoint3=Endpoint.publish("http://localhost:50003/cal", webLib3);
+		System.out.println("Concordia : "+ endpoint1.isPublished());
+		System.out.println("Ottawa : "+ endpoint2.isPublished());
+		System.out.println("Waterloo : "+ endpoint3.isPublished());
+	}
+
+}
